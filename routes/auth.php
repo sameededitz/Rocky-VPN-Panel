74323a6e9b6873fd113bafab2e09b79cd8e3f415
Sameed <sameedhassan22@gmail.com>
2025-04-03 18:32:26 +0500

    Implement logout functionality using Livewire; update routes to use new Logout action; clean up unused code in navbar and vps-manager views.

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,7 @@
 use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VerifyController;
+use App\Livewire\Actions\Logout;
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', Login::class)->name('login');
@@ -10,7 +11,7 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/email/verify/{id}/{hash}', [VerifyController::class, 'verify'])->middleware(['signed', 'throttle:6,1'])->withoutMiddleware(['auth'])->name('verification.verify');
 
-    // Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/logout', Logout::class)->name('logout');
 });
 
 Route::get('email/verify/view/{id}/{hash}', [VerifyController::class, 'viewEmail'])->name('email.verification.view');
